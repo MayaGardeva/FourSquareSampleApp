96b84cb101612f4b574393742a36b0e55a469a65
Maya Gardeva <mayagurdeva@gmail.com>
2018-02-11 17:00:22 +0000

    create repository

---
+++ app/src/main/java/com/api/foursquare/VenuesActivity.java
@@ -0,0 +1,40 @@
+package com.api.foursquare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.api.foursquare.ui.custom_views.EndlessRecyclerView;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by mayagardeva on 11/02/2018.
+ */
+
+public class VenuesActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar_view) Toolbar toolbar;
+    @BindView(R.id.recycler_view) EndlessRecyclerView recyclerView;
+    @BindView(R.id.search_view) MaterialSearchView searchView;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_recommendations);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/api/foursquare/ui/custom_views/EndlessRecyclerView.java
@@ -0,0 +1,87 @@
+package com.api.foursquare.ui.custom_views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by mayagardeva on 11/02/2018.
+ */
+
+public class EndlessRecyclerView extends RecyclerView {
+    private AdvancedEndlessRecyclerOnScrollListener onScrollListener;
+
+    public EndlessRecyclerView(Context context) {
+        super(context);
+    }
+
+    public EndlessRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EndlessRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override public void setLayoutManager(LayoutManager layout) {
+        if (!(layout instanceof LinearLayoutManager)) {
+            throw new RuntimeException();
+        }
+
+        onScrollListener = new AdvancedEndlessRecyclerOnScrollListener(layout) {
+            @Override public void onLoadMore() {
+                if (onLoadMoreListener != null) {
+                    onLoadMoreListener.onLoadMore();
+                }
+            }
+        };
+
+        addOnScrollListener(onScrollListener);
+        super.setLayoutManager(layout);
+    }
+
+    private onLoadMoreListener onLoadMoreListener;
+
+    public void setOnLoadMoreListener(EndlessRecyclerView.onLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+    public interface onLoadMoreListener {
+        void onLoadMore();
+    }
+
+    public void setLoading(boolean enable) {
+        this.onScrollListener.setLoading(enable);
+    }
+
+    public abstract class AdvancedEndlessRecyclerOnScrollListener extends OnScrollListener {
+        private int visibleThreshold = 3;
+        private int lastVisibleItem, totalItemCount;
+        private boolean loading;
+        private LayoutManager linearLayoutManager;
+
+        public AdvancedEndlessRecyclerOnScrollListener(LayoutManager linearLayoutManager) {
+            this.linearLayoutManager = linearLayoutManager;
+        }
+
+        @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            totalItemCount = linearLayoutManager.getItemCount();
+            lastVisibleItem = ((LinearLayoutManager) linearLayoutManager).findLastVisibleItemPosition();
+
+            if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                loading = true;
+                onLoadMore();
+            }
+        }
+
+        public void setLoading(boolean enable) {
+            this.loading = enable;
+        }
+
+        public abstract void onLoadMore();
+    }
+}
